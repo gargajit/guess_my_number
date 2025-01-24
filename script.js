d1b97b3bdfc7c866d64791bdf94a2e5c67fcb157
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2025-01-24 12:43:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,48 +2,57 @@
 
 let defaultMessage = document.querySelector('.message').textContent;
 let defaultNumber = document.querySelector('.number').textContent;
-let defaultScore = document.querySelector('.score').textContent;
+let defaultScore = 20;
 let defaultGuess = document.querySelector('.guess').value;
 
 // console.log(typeof defaultGuess);
 
 const check = document.querySelector('.check');
-let score = Number(document.querySelector('.score').innerHTML);
+let score = defaultScore;
 
 let highscore = 0;
 
-const randNum = Math.floor(Math.random() * 20) + 1;
+let randNum = Math.floor(Math.random() * 20) + 1;
 console.log(randNum);
 
 check.addEventListener('click', function () {
   const inputNum = document.querySelector('.guess').value;
 
+  if (!inputNum) {
+    document.querySelector('.message').textContent = '⛔ No Number!';
+    return;
+  }
+
   if (inputNum == randNum) {
     document.body.style.backgroundColor = '#60b347';
-    document.querySelector('.message').innerHTML = '🎉 Correct Number!';
-    document.querySelector('.number').innerHTML = randNum;
-    // document.querySelector('.highscore').innerHTML = score;
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('.number').textContent = randNum;
+    // document.querySelector('.highscore').textContent = score;
     if (score > highscore) {
       highscore = score;
-      document.querySelector('.highscore').innerHTML = highscore;
+      document.querySelector('.highscore').textContent = highscore;
     }
   } else if (inputNum < randNum) {
-    document.querySelector('.message').innerHTML = '📉 Too Low';
-    score = Number(document.querySelector('.score').innerHTML) - 1;
-    document.querySelector('.score').innerHTML = score;
+    document.querySelector('.message').textContent = '📉 Too Low';
+    score = Number(document.querySelector('.score').textContent) - 1;
+    document.querySelector('.score').textContent = score;
   } else {
-    document.querySelector('.message').innerHTML = '📈 Too High';
-    score = Number(document.querySelector('.score').innerHTML) - 1;
-    document.querySelector('.score').innerHTML = score;
+    document.querySelector('.message').textContent = '📈 Too High';
+    score = Number(document.querySelector('.score').textContent) - 1;
+    document.querySelector('.score').textContent = score;
   }
 });
 
 const againBtn = document.querySelector('.again');
 
 againBtn.addEventListener('click', function () {
+  score = defaultScore;
+  randNum = Math.floor(Math.random() * 20) + 1;
+  console.log(randNum);
+
   document.body.style.backgroundColor = '#222';
   document.querySelector('.number').textContent = defaultNumber;
   document.querySelector('.message').textContent = defaultMessage;
-  document.querySelector('.score').textContent = defaultScore;
+  document.querySelector('.score').textContent = score;
   document.querySelector('.guess').value = defaultGuess;
 });
